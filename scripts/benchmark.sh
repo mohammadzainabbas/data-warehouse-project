a19f556827daeab57447374e8289ed71baa2de58
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2021-10-28 14:00:37 +0200

    Work in progress

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,22 +1,65 @@
 #!/bin/bash
+#====================================================================================
+# Author: Mohammad Zain Abbas
+# Date: 28th Oct, 2021
+#====================================================================================
+# This script is used to benchmark via TPC-DS using some scale.
+#====================================================================================
 
+# Enable exit on error
 set -e -u -o pipefail
 
-say() {
-    echo $1
-}
-
-log() {
-    say "[[ log ]] $1"
-}
-
-error() {
-    say "[[ error ]] $1" 
+log () {
+    echo "[[ log ]] $1"
 }
 
 line_separator() {
-    say "\n========================================\n"
+    echo "\n========================================\n"
 }
+
+#Function that shows usage for this script
+function usage()
+{
+cat << HEREDOC
+
+Benchmark for a given scale using TPC-DS benchmark
+
+Usage: 
+    
+    $progname [OPTION] [Value]
+
+Options:
+
+    -s, --scale             Scale in Gb. (by default 1)
+    -p, --path              Path for tpcds directory. (required)
+    -h, --help              Show usage
+
+Examples:
+
+    $ $progname -s 1
+    ⚐ → Benchmark for 1 Gb scale.
+
+    $ $progname -s 5 -p ../tpcds-kit/
+    ⚐ → Benchmark for 5 Gb scale with tpcds dir path as '../tpcds-kit'ß.
+
+HEREDOC
+}
+
+scale=1
+
+#Get all the arguments and update accordingly
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -s|--scale) scale="$2"; shift ;;
+        -p|--path) path="$2"; shift ;;
+        -h|--help)
+        usage
+        exit 1
+        ;;
+        *) printf "\n$progname: invalid option → '$1'\n\n⚐ Try '$progname -h' for more information\n\n"; exit 1 ;;
+    esac
+    shift
+done
 
 benchmark() {
     local scale_factor=$1
@@ -40,7 +83,6 @@ benchmark() {
 #--------------------
 # Configurations
 #--------------------
-benchmark_scales=(1 5 10 25 50)
 
 log "Starting Benchmarking Service"
 
