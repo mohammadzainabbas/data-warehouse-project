7fe9533bc9577884ffae81d86f71e9caf8adea1e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2021-10-28 14:56:35 +0200

    Updated to use the benchmark script

--- scripts/benchmark_all.sh
+++ scripts/benchmark_all.sh
@@ -1,49 +1,18 @@
 #!/bin/bash
+#====================================================================================
+# Author: Mohammad Zain Abbas
+# Date: 28th Oct, 2021
+#====================================================================================
+#  This script is used to benchmark via TPC-DS for all scales.
+#====================================================================================
 
+# Enable exit on error
 set -e -u -o pipefail
 
-say() {
-    echo $1
-}
+# import helper functions from 'utils.sh'
+source utils.sh
 
-log() {
-    say "[[ log ]] $1"
-}
-
-error() {
-    say "[[ error ]] $1" 
-}
-
-line_separator() {
-    say "\n========================================\n"
-}
-
-benchmark() {
-    local scale_factor=$1
-
-    line_separator
-
-    log "Scale factor: $scale_factor Gb"
-
-    log "Generating data for $scale_factor"
-    sh scripts/generate_data.sh -s $scale_factor -p ../tpcds-kit
-    
-    log "Generating queries for $scale_factor"
-    sh scripts/generate_queries.sh -s $scale_factor -p ../tpcds-kit
-
-    # @todo: make adjustment for modified queries' path -> save in same locations (and save the old versions somewhere else)
-
-    log "Benchmarking for $scale_factor"
-    spark-submit --jars ~/Downloads/mysql-connector-java-8.0.26/mysql-connector-java-8.0.26.jar scripts/run_queries.py -scale $scale_factor
-}
-
-#--------------------
-# Configurations
-#--------------------
+# scale factors to be used
 benchmark_scales=(1 5 10 25 50)
 
-log "Starting Benchmarking Service"
-
-for i in "${benchmark_scales[@]}"; do benchmark "$i"; done
-
-log "All done !!"
+for i in "${benchmark_scales[@]}"; do sh benchmark.sh "$i"; done
