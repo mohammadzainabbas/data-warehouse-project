dde5e780816ffe5cefb03500b3ff6b1dd37049c2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2021-10-29 21:57:24 +0200

    Updated python version (spark is not supported yet for 3.9 and above)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -113,7 +113,7 @@ install_conda() {
 }
 
 create_conda_env() {
-    conda create -n $env_name python=3.9 pandas -y || error "Unable to create new env '$env_name' ..."
+    conda create -n $env_name python=3.8 pandas -y || error "Unable to create new env '$env_name' ..."
     conda activate $env_name &> /dev/null || echo "" > /dev/null
     pip install pyspark > /dev/null
 }
